76d9d4d6b1f357af309dde15448a4ae0a7054786
Mohammadali Erfani <mohammadali.erfani@stud.th-deg.de>
2022-03-29 23:53:17 +0200

    added click functionablity to h2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+document.addEventListener('click', function (event) {
+    console.log("into the click event")
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.headtwo')) return;
+	// Don't follow the link
+	event.preventDefault();
+	// Log the clicked element in the console
+	console.log("Hello world");
+
+}, false);
